1d93ebde3ba8d1f02170f9886d108a66ca868173
Precious Iheanyi <emmypresh777@gmail.com>
2023-10-01 19:28:16 +0100

    Update config.py

--- resources/config.py
+++ resources/config.py
@@ -5,5 +5,5 @@ class Config():
     API_TITLE = "Chrome Extension API"
     API_VERSION = "v1"
     OPENAPI_VERSION = "3.0.3"
-    MAX_CONTENT_LENGTH = 1000 * 1024 * 1024
+    MAX_CONTENT_LENGTH = 100 * 1024 * 1024
     UPLOAD_FOLDER = os.path.join(os.getcwd(), "uploads")
