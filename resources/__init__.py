77a7c583ad9ee959ee4196eb7f290b3c58aef4a6
Precious Iheanyi <emmypresh777@gmail.com>
2023-10-01 17:00:04 +0100

    Update __init__.py

--- resources/__init__.py
+++ resources/__init__.py
@@ -1,9 +1,7 @@
 from flask import Flask, jsonify
 from resources.config import Config
 from dotenv import load_dotenv
-# from flask_smorest import Api
-from flask_restful import Api 
-# from resources.views import blp as VideoBlueprint
+from flask_restful import Api
 from resources.handles import VideoListResource, VideoToDisk, TranscribeVideo
 import os
 from flask_cors import CORS
@@ -22,7 +20,6 @@ def create_app():
     def hello():
         return jsonify({"greeting": "Hello World"})
 
-    # api.register_blueprint(VideoBlueprint)
     api.add_resource(VideoListResource, "/videos")
     api.add_resource(VideoToDisk, "/videos/upload")
     api.add_resource(TranscribeVideo, "/videos/<filename>/transcribe")
